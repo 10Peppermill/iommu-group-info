30ab3c8fed5d90af7d6d64119605c09176298dbc
10Peppermill <cohen.jon@husky.neu.edu>
2023-04-08 16:20:35 -0400

    Refactor for improved formatting.

--- iommu_viewer.sh
+++ iommu_viewer.sh
@@ -1,53 +1,54 @@
 #!/bin/bash
 
+# Print a message to the console to inform the user that the script is running.
 echo "Please be patient. This may take a couple seconds."
 
+# Declare an associative array to hold the lines for each IOMMU group, and set the field_delimiter to "|".
+declare -A iommu_group_lines
+field_delimiter="|"
 
+# Loop through each IOMMU group.
+for iommu_group in $(find /sys/kernel/iommu_groups/ -maxdepth 1 -mindepth 1 -type d | sed 's/.*iommu_groups\///'); do
+    # For each device in the group, add its lspci output to the group's lines in the associative array.
+    for iommu_group_device in $(find /sys/kernel/iommu_groups/$iommu_group/devices/* -maxdepth 0 -mindepth 0 2>/dev/null); do
+        iommu_group_lines[$iommu_group]+="$(lspci -Dnns ${iommu_group_device##*/})$field_delimiter"
+    done
+done
 
-	#Initializing the list of all IOMMU groups
-	GROUP=`find /sys/kernel/iommu_groups/ -type l | cut -d '/' -f 5,7 --output-delimiter='-'`
+# Loop through each IOMMU group's lines and print them out with the group number, indenting if necessary.
+for iommu_group_key in $(echo "${!iommu_group_lines[@]}" | tr ' ' '\n' | sort -n); do
+    if [ $iommu_group_key -lt 10 ]; then
+        group_indent=" "
+    else
+        group_indent=""
+    fi
 
+    # Print out the group number with the appropriate group_indent.
+    printf 'Group:\t%s%s' "$group_indent" "$iommu_group_key"
 
-	for i in $GROUP; do
+    # Loop through each line in the group and print it out.
+    is_first_line="true"
+    while IFS="$field_delimiter" read -ra device_lines; do
+        for device_line in "${device_lines[@]}"; do
+            # Extract the part of the lspci output that contains the device address.
+            device_addr=$(echo "$device_line" | cut -d' ' -f1)
 
+            # Use lspci to get the kernel driver in use for the device, and remove any leading or trailing whitespace.
+            kernel_driver=$(lspci -k -s $device_addr | grep "Kernel driver in use" | cut -d ':' -f 2 | tr -d '\n' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
 
-		#K holds the group number
-		k=`echo $i | cut -d '-' -f 1`
+            # Print out the line, indenting if necessary.
+            if [ "$is_first_line" = "true" ]; then
+                printf '  %s' "$device_line"
+                is_first_line="false"
+            else
+                printf '\n\t    %s' "$device_line"
+            fi
 
-		#L holds the address
-		l=`echo $i | cut -d '-' -f 2`
-
-		#J holds the part of the address that's pasted into lspci to get the name
-		j=`echo $i | cut -d ':' -f 2,3,4`
-		
-		#M holds the kernel driver in use
-		m=`lspci -k -s $j | grep "Kernel driver in use"`
-		
-		echo -n  "Group: "
-
-		#This if-statement is here for proper alignment. If group is less than 10, a space is added.
-		if [ $k -lt 10 ]
-			then
-				echo -n " $k  "
-			else
-				echo -n " $k "
-		fi
-
-		#Outputting the address
-		echo -n " $l "
-
-		#Outputting the name and id
-		echo -n "`lspci -nn | grep $j | cut -d ' ' -f 2-`"
-		
-		#Only displays "   Driver:" if m is not an empty string
-		if ! [ -z "$m" ]
-			then
-				echo -n "   Driver:"
-		fi
-		
-		#Outputting the kernel driver in use
-		echo "$m" | cut -d ':' -f 2
-
-
-	#The output is sorted numerically based on the second space-separated field.
-	done | sort -nk2
+            # If there is a kernel driver in use for the device, print it out with a "Driver:" label.
+            if ! [ -z "$kernel_driver" ]; then
+                printf '   Driver: %s' "$kernel_driver"
+            fi
+        done
+    done <<< "${iommu_group_lines[$iommu_group_key]}"
+    printf '\n'
+done
